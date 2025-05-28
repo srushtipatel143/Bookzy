457bce94f905ad165bb47303b0a0fc542a6dfc67
Srushtipatel14 <srushtip579@gmail.com>
2025-05-28 17:57:48 +0530

    done

--- uiService/app/components/seat/noOfSeat.tsx
+++ uiService/app/components/seat/noOfSeat.tsx
@@ -1,19 +1,32 @@
 import { Modal } from "react-bootstrap";
+import Image from 'next/image';
 
-const SeatNomodal=()=> {
+const SeatNomodal = () => {
 
-  return (
-    <div>
-      <Modal show={true} centered contentClassName="custom_modal">
-        <Modal.Header className="border-0" >
-        
-        </Modal.Header>
-        <Modal.Body>
-         
-        </Modal.Body>
-      </Modal>
-    </div>
-  )
+    return (
+        <div>
+            <Modal show={true} centered contentClassName="custom_modal">
+                <Modal.Header className="border-0" >
+
+                </Modal.Header>
+                <Modal.Body>
+                    <div className="d-flex flex-column justify-content-center align-items-center">
+                        <div>How Many Seats?</div>
+                        <div className="mt-4">
+                            <Image
+                                src="/scooter.webp"
+                                alt="movie"
+                                height={120}
+                                width={120}
+                                priority
+                                
+                            />
+                        </div>
+                    </div>
+                </Modal.Body>
+            </Modal>
+        </div>
+    )
 }
 
 export default SeatNomodal;
