dca49beaf7a98a2c70fbb54daebdf7e5a3007ad2
Srushtipatel14 <srushtip579@gmail.com>
2025-07-04 17:53:39 +0530

    movie add at frontend side

--- movieService/src/controllers/admin/adminMovie.js
+++ movieService/src/controllers/admin/adminMovie.js
@@ -1,36 +1,68 @@
 const errorHandler = require("../../helpers/errors/errorHandler");
 const Movie = require("../../models/movieInfoModel");
-const Show=require("../../models/showInformationModel");
+const Show = require("../../models/showInformationModel");
+
+const formatDate = (input) => {
+    const date = new Date(input);
+    const day = date.getDate();
+    const month = date.toLocaleString('en-US', { month: 'short' });
+    const year = date.getFullYear();
+    return `${day}-${month}-${year}`;
+};
 
 const addMovie = async (req, res, next) => {
     try {
         const data = req.body;
         const { id } = req.admin;
+        const movieType = JSON.parse(data.movieType);
+        const movieLanguage = JSON.parse(data.movieLanguage);
+        const cast = JSON.parse(data.cast);
+        const formatted = formatDate(data.releaseDate)
 
-        const movie = new Movie({ ...data, userId: id });
+        cast.forEach(actor => {
+            if (actor.imageUrl?.trim() === '') {
+                delete actor.imageUrl;
+            }
+        });
+
+        const movieDataVal = {
+            ...data,
+            releaseDate: formatted,
+            movieType,
+            movieLanguage,
+            cast,
+            userId: id
+        };
+
+        const movie = new Movie(movieDataVal);
         const movieData = await movie.save();
+        const formattedResponse = {
+            ...movieData.toObject(),
+            releaseDate: formatDate(movieData.releaseDate)
+        };
 
         return res.status(200).json({
             success: true,
             message: "Movie added successfully",
-            data: movieData
+            data: formattedResponse
         });
     } catch (error) {
+        console.log(error)
         return next(new errorHandler("Something went wrong", 500, error));
     }
 }
 
 const updateMovie = async (req, res, next) => {
     try {
-        const data = req.body; 
+        const data = req.body;
         const { id } = req.admin;
 
-        const movie=await Movie.findById({_id:data._id})
-        if(!movie) return next(new errorHandler("Movie not found", 401));
-        if (movie.userId.toString() !== id)  return next(new errorHandler("User is not authorized", 403));
-        await Movie.updateOne({_id:data._id},{$set:data});
-        if(movie.title!==data.title){
-             await Show.updateMany({movieId:data._id},{$set:{movieName:data.title}})
+        const movie = await Movie.findById({ _id: data._id })
+        if (!movie) return next(new errorHandler("Movie not found", 401));
+        if (movie.userId.toString() !== id) return next(new errorHandler("User is not authorized", 403));
+        await Movie.updateOne({ _id: data._id }, { $set: data });
+        if (movie.title !== data.title) {
+            await Show.updateMany({ movieId: data._id }, { $set: { movieName: data.title } })
         }
         return res.status(200).json({
             success: true,
@@ -45,11 +77,16 @@ const updateMovie = async (req, res, next) => {
 const getAllMovie = async (req, res, next) => {
     try {
         const userId = req.admin.id;
-        const getAllMovie = await Movie.find({userId:userId});
+        const getAllMovie = await Movie.find({ userId: userId });
+        const formattedMovies = getAllMovie.map((item) => {
+            const movie = item.toObject();
+            movie.releaseDate = formatDate(item.releaseDate);
+            return movie;
+        });
         return res.status(200).json({
             success: true,
             message: "Movie get successfully",
-            data: getAllMovie
+            data: formattedMovies
         });
 
     } catch (error) {
@@ -61,8 +98,8 @@ const getSingleMovie = async (req, res, next) => {
     try {
         const { id } = req.params;
         const userId = req.admin.id;
-        const getMovie = await Movie.findOne({ _id: id,userId:userId });
-        if(!getMovie){
+        const getMovie = await Movie.findOne({ _id: id, userId: userId });
+        if (!getMovie) {
             return next(new errorHandler("Movie not found", 401));
         }
         return res.status(200).json({
