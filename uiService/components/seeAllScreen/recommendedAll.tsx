88d0636fb2dd474b3b5bf10131e06c2d1e8862c7
Srushtipatel14 <srushtip579@gmail.com>
2026-05-03 23:09:10 +0530

    make layout responsive

--- uiService/components/seeAllScreen/recommendedAll.tsx
+++ uiService/components/seeAllScreen/recommendedAll.tsx
@@ -168,7 +168,7 @@ const RecommendedAll = () => {
                     <div className="see_scroll">
                         {movieData.map((item) => (
                             <div key={item._id} className="see-card p-0" onClick={() => router.push(`/explore/movie/${item._id}`)}>
-                                <div style={{ height: "400px" }} className="mb-3">
+                                <div className="mb-3">
                                     <div className="recommendedAllMovie_wrapper" style={{ position: 'relative', height: "80%", width: "100%" }}>
                                         <Image
                                             src={item?.movieImageURl}
