63c06edf7e456d344eb39ac2c3f59ac44081beae
Srushtipatel14 <srushtip579@gmail.com>
2026-05-05 21:51:17 +0530

    solve db conn issue

--- movieService/src/config/dbConn.js
+++ movieService/src/config/dbConn.js
@@ -5,13 +5,16 @@ const mysql=require("mysql2");
 const MONGO_URL=process.env.MONGO_URL;
 mongoose.connect(MONGO_URL)
 
-const pool=mysql.createPool({
-   host:process.env.HOST,
-   user:process.env.USER,
-   password:process.env.PASSWORD,
-   database:process.env.DATABASE,
-   connectionLimit:10,
-   waitForConnections: true,
+const pool = mysql.createPool({
+  host: process.env.HOST,
+  port: process.env.DBPORT,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  connectionLimit: 10,
+  waitForConnections: true,
 }).promise();
 
 module.exports={pool,MONGO_URL,mongoose};
+
+
