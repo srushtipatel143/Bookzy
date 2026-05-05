c5a5969daca9436d384684b3b30f4997748e5379
Srushtipatel14 <srushtip579@gmail.com>
2026-05-05 10:47:11 +0530

    done

--- uiService/app/explore/movie/[id]/page.tsx
+++ uiService/app/explore/movie/[id]/page.tsx
@@ -1,22 +1,16 @@
 import { API_USER_URL } from "@/utils/config";
 import Moviescreen from "@/components/movie/movie";
-interface PageProps {
-    params: {
-        id: string
-    }
-}
 
 const getMovieDetails = async (id: string) => {
-    const res = await fetch(`${API_USER_URL}/getSingleMovie/${id}`);
-    if (!res.ok) throw new Error("Failed to fetch movie");
-    const json = await res.json();
-    return json.data;
+  const res = await fetch(`${API_USER_URL}/getSingleMovie/${id}`, {
+    cache: "no-store",
+  });
+  if (!res.ok) throw new Error("Failed to fetch movie");
+  const json = await res.json();
+  return json.data;
+};
+
+export default async function Movie({ params }: any) {
+  const movie = await getMovieDetails(params.id);
+  return <Moviescreen movie={movie} />;
 }
-const Movie = async ({ params }: PageProps) => {
-    const { id } = await params;
-    const movie = await getMovieDetails(id);
-    return (
-        <Moviescreen movie={movie} />
-    )
-}
-export default Movie;
