ad04e2147388ff61d3b9eb738265e84d6880deb0
Srushtipatel14 <srushtip579@gmail.com>
2026-05-05 14:56:38 +0530

    done

--- uiService/app/explore/movie/[id]/page.tsx
+++ uiService/app/explore/movie/[id]/page.tsx
@@ -13,9 +13,11 @@ const getMovieDetails = async (id: string) => {
 export default async function Movie({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
-  const movie = await getMovieDetails(params.id);
+  const { id } = await params;
+
+  const movie = await getMovieDetails(id);
 
   return <Moviescreen movie={movie} />;
 }
