5311932d490b1b3d3a4330accae22bcfce010488
Srushtipatel14 <srushtip579@gmail.com>
2026-05-06 10:34:13 +0530

    add log in mailing

--- authService/src/helpers/libraries/sendEmail.js
+++ authService/src/helpers/libraries/sendEmail.js
@@ -1,17 +1,42 @@
+// const nodemailer = require("nodemailer");
+// require("dotenv").config();
+
+// const sendEmail = async (mailOptions) => {
+//     try {
+//         const transporter = nodemailer.createTransport({
+//             host: process.env.SMTP_HOST,
+//             port: Number(process.env.SMTP_PORT),
+//             secure: false,
+//             auth: {
+//                 user: process.env.EMAIL_USERNAME,
+//                 pass: process.env.EMAIL_PASS,
+//             },
+//             connectionTimeout: 10000,
+//         });
+
+//         const info = await transporter.sendMail(mailOptions);
+
+//         console.log("Email sent:", info.response);
+
+//     } catch (error) {
+//         console.error("MAIL ERROR:", error);
+//         throw error;
+//     }
+// };
+
+// module.exports = sendEmail;
+
+
 const nodemailer = require("nodemailer");
-require("dotenv").config();
 
 const sendEmail = async (mailOptions) => {
     try {
         const transporter = nodemailer.createTransport({
-            host: process.env.SMTP_HOST,
-            port: Number(process.env.SMTP_PORT),
-            secure: false,
+            service: "gmail",
             auth: {
                 user: process.env.EMAIL_USERNAME,
                 pass: process.env.EMAIL_PASS,
             },
-            connectionTimeout: 10000,
         });
 
         const info = await transporter.sendMail(mailOptions);
